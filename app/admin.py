bd4df8f73e1bc336f0a6eba8032ffd45a949cb7e
jmorin42 <jacobim2056@gmail.com>
2024-11-18 19:41:06 -0600

    add and view tasks

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from .models import Task
+admin.site.unregister(Group)
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username", "first_name", "last_name", "email", "password"]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Task)
